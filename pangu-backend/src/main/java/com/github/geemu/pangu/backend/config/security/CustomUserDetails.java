02f408cd7c25c3e7144bfcbdeba54f633e4db635
geemu <Boy.cfm*#520>
2022-02-19 19:35:25 +0800

    1

--- pangu-backend/src/main/java/com/github/geemu/pangu/backend/config/security/CustomUserDetails.java
+++ pangu-backend/src/main/java/com/github/geemu/pangu/backend/config/security/CustomUserDetails.java
@@ -1,110 +1,110 @@
-package com.github.geemu.pangu.backend.config.security;
-
-import org.springframework.security.core.CredentialsContainer;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.userdetails.UserDetails;
-
-import java.util.Set;
-
-/**
- * CustomUserDetails
- * @author 陈方明  cfmmail@sina.com
- * @since 2019-01-01 14:58
- */
-public class CustomUserDetails implements UserDetails, CredentialsContainer {
-
-    /** 用户id **/
-    private Long id;
-    /** 用户名  忽略大小写 **/
-    private String username;
-    /** 密码  区分大小写 **/
-    private String password;
-    /** 是否可用  0不可用  1可用 **/
-    private Boolean enabled;
-    /** 角色id集合 **/
-    private Set<GrantedAuthority> authorities;
-
-    public CustomUserDetails() {
-    }
-
-    public CustomUserDetails(Long id, String username, String password, Boolean enabled, Set<GrantedAuthority> authorities) {
-        this.id = id;
-        this.username = username;
-        this.password = password;
-        this.enabled = enabled;
-        this.authorities = authorities;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    @Override
-    public Set<GrantedAuthority> getAuthorities() {
-        return authorities;
-    }
-
-    @Override
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    @Override
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    @Override
-    public boolean isAccountNonExpired() {
-        return true;
-    }
-
-    @Override
-    public boolean isAccountNonLocked() {
-        return true;
-    }
-
-    @Override
-    public boolean isCredentialsNonExpired() {
-        return true;
-    }
-
-    public Boolean getEnabled() {
-        return enabled;
-    }
-
-    public void setEnabled(Boolean enabled) {
-        this.enabled = enabled;
-    }
-
-    @Override
-    public boolean isEnabled() {
-        return enabled;
-    }
-
-    public void setEnabled(boolean enabled) {
-        this.enabled = enabled;
-    }
-
-    public void setAuthorities(Set<GrantedAuthority> authorities) {
-        this.authorities = authorities;
-    }
-
-    @Override
-    public void eraseCredentials() {
-        this.password = null;
-    }
-
-}
+//package com.github.geemu.pangu.backend.config.security;
+//
+//import org.springframework.security.core.CredentialsContainer;
+//import org.springframework.security.core.GrantedAuthority;
+//import org.springframework.security.core.userdetails.UserDetails;
+//
+//import java.util.Set;
+//
+///**
+// * CustomUserDetails
+// * @author 陈方明  cfmmail@sina.com
+// * @since 2019-01-01 14:58
+// */
+//public class CustomUserDetails implements UserDetails, CredentialsContainer {
+//
+//    /** 用户id **/
+//    private Long id;
+//    /** 用户名  忽略大小写 **/
+//    private String username;
+//    /** 密码  区分大小写 **/
+//    private String password;
+//    /** 是否可用  0不可用  1可用 **/
+//    private Boolean enabled;
+//    /** 角色id集合 **/
+//    private Set<GrantedAuthority> authorities;
+//
+//    public CustomUserDetails() {
+//    }
+//
+//    public CustomUserDetails(Long id, String username, String password, Boolean enabled, Set<GrantedAuthority> authorities) {
+//        this.id = id;
+//        this.username = username;
+//        this.password = password;
+//        this.enabled = enabled;
+//        this.authorities = authorities;
+//    }
+//
+//    public Long getId() {
+//        return id;
+//    }
+//
+//    public void setId(Long id) {
+//        this.id = id;
+//    }
+//
+//    @Override
+//    public Set<GrantedAuthority> getAuthorities() {
+//        return authorities;
+//    }
+//
+//    @Override
+//    public String getPassword() {
+//        return password;
+//    }
+//
+//    public void setPassword(String password) {
+//        this.password = password;
+//    }
+//
+//    @Override
+//    public String getUsername() {
+//        return username;
+//    }
+//
+//    public void setUsername(String username) {
+//        this.username = username;
+//    }
+//
+//    @Override
+//    public boolean isAccountNonExpired() {
+//        return true;
+//    }
+//
+//    @Override
+//    public boolean isAccountNonLocked() {
+//        return true;
+//    }
+//
+//    @Override
+//    public boolean isCredentialsNonExpired() {
+//        return true;
+//    }
+//
+//    public Boolean getEnabled() {
+//        return enabled;
+//    }
+//
+//    public void setEnabled(Boolean enabled) {
+//        this.enabled = enabled;
+//    }
+//
+//    @Override
+//    public boolean isEnabled() {
+//        return enabled;
+//    }
+//
+//    public void setEnabled(boolean enabled) {
+//        this.enabled = enabled;
+//    }
+//
+//    public void setAuthorities(Set<GrantedAuthority> authorities) {
+//        this.authorities = authorities;
+//    }
+//
+//    @Override
+//    public void eraseCredentials() {
+//        this.password = null;
+//    }
+//
+//}
--- pangu-backend/src/main/java/com/github/geemu/pangu/backend/config/security/SecurityUtils.java
+++ pangu-backend/src/main/java/com/github/geemu/pangu/backend/config/security/SecurityUtils.java
@@ -1,64 +1,64 @@
-package com.github.geemu.pangu.backend.config.security;
-
-import org.springframework.security.authentication.AnonymousAuthenticationToken;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContext;
-import org.springframework.security.core.context.SecurityContextHolder;
-
-/**
- * SecurityUtils
- * @author 陈方明  cfmmail@sina.com
- * @since 2019-03-30 11:10
- */
-public final class SecurityUtils {
-
-    /**
-     * 工具类隐藏构造
-     */
-    private SecurityUtils() {
-        // ignore private construct
-    }
-
-    /**
-     * 获取当前用户 如果为匿名用户返回null
-     * @return CustomUserDetails
-     */
-    public static CustomUserDetails getCurrentUser() {
-        SecurityContext context = SecurityContextHolder.getContext();
-        if (null == context) {
-            return null;
-        }
-        Authentication auth = context.getAuthentication();
-        if (null == auth) {
-            return null;
-        }
-        if (auth instanceof AnonymousAuthenticationToken) {
-            return null;
-        }
-        Object principal = auth.getPrincipal();
-        if (null == principal) {
-            return null;
-        }
-        return (CustomUserDetails) principal;
-    }
-
-    /**
-     * 获取当前用户 如果为匿名用户返回null
-     * @return CustomUserDetails
-     */
-    public static CustomUserDetails getCurrentUser(Authentication auth) {
-        if (null == auth) {
-            return null;
-        }
-        if (auth instanceof AnonymousAuthenticationToken) {
-            return null;
-        }
-        Object principal = auth.getPrincipal();
-        if (null == principal) {
-            return null;
-        }
-        return (CustomUserDetails) principal;
-    }
-
-
-}
+//package com.github.geemu.pangu.backend.config.security;
+//
+//import org.springframework.security.authentication.AnonymousAuthenticationToken;
+//import org.springframework.security.core.Authentication;
+//import org.springframework.security.core.context.SecurityContext;
+//import org.springframework.security.core.context.SecurityContextHolder;
+//
+///**
+// * SecurityUtils
+// * @author 陈方明  cfmmail@sina.com
+// * @since 2019-03-30 11:10
+// */
+//public final class SecurityUtils {
+//
+//    /**
+//     * 工具类隐藏构造
+//     */
+//    private SecurityUtils() {
+//        // ignore private construct
+//    }
+//
+//    /**
+//     * 获取当前用户 如果为匿名用户返回null
+//     * @return CustomUserDetails
+//     */
+//    public static CustomUserDetails getCurrentUser() {
+//        SecurityContext context = SecurityContextHolder.getContext();
+//        if (null == context) {
+//            return null;
+//        }
+//        Authentication auth = context.getAuthentication();
+//        if (null == auth) {
+//            return null;
+//        }
+//        if (auth instanceof AnonymousAuthenticationToken) {
+//            return null;
+//        }
+//        Object principal = auth.getPrincipal();
+//        if (null == principal) {
+//            return null;
+//        }
+//        return (CustomUserDetails) principal;
+//    }
+//
+//    /**
+//     * 获取当前用户 如果为匿名用户返回null
+//     * @return CustomUserDetails
+//     */
+//    public static CustomUserDetails getCurrentUser(Authentication auth) {
+//        if (null == auth) {
+//            return null;
+//        }
+//        if (auth instanceof AnonymousAuthenticationToken) {
+//            return null;
+//        }
+//        Object principal = auth.getPrincipal();
+//        if (null == principal) {
+//            return null;
+//        }
+//        return (CustomUserDetails) principal;
+//    }
+//
+//
+//}
