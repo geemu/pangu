6caed85ac2617aa6281201f86ad6b4ec30ea643e
chenfangming <cfmmail@sina.com>
2022-12-16 20:57:37 +0800

    init project

---
+++ pangu-backend/src/main/java/com/github/geemu/pangu/backend/core/http/ResponseStatus.java
@@ -0,0 +1,14 @@
+package com.github.geemu.pangu.backend.core.http;
+
+/**
+ * com.github.geemu.pangu.backend.core.http
+ * @author 陈方明  cfmmail@sina.com
+ * @since 2022-12-16 20:56
+ */
+public interface ResponseStatus {
+
+    int getCode();
+
+    String getMessage();
+
+}
